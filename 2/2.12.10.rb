ac99837de5210f01e1019b597ed16615f017638c
Otegami <a.s.takuya1026@gmai.com>
2019-07-04 14:46:42 +0900

    done : section2

---
+++ 2/2.12.10.rb
@@ -0,0 +1,21 @@
+puts 123
+puts 'abc'
+
+print 123
+print "abc"
+
+p 123
+p 'abc'
+
+puts  "------------"
+
+s = "abc\ndef"
+puts s
+print s
+puts ''
+p s
+
+a = [ 1, 2, 3]
+puts a
+print a
+p a 
---
+++ 2/2.12.4.rb
@@ -0,0 +1,6 @@
+
+true = 1
+
+puts __FILE__
+puts __LINE__
+puts __ENCODING__
